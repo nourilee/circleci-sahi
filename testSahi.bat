364b4c868a0f3d505feb6750ac45386224e290e6
Nourilee <nourilee@gmail.com>
2019-02-06 10:05:42 +0800

    Trial -- 20190602

--- testSahi.bat
+++ testSahi.bat
@@ -1,5 +1,12 @@
+:: This will start Git bash shell scripting
+cd C:\Program Files\Git\bin && start sh.exe
+
+:: This will execute git commands
+cd C:/Users/Administrator/Documents/GitHub/circleci-sahi && git remote -v 
+exit
+
 :: This will copy scripts from repository to the sahi directory
-:: xcopy /s C:\Users\Administrator\Documents\GitHub\circleci-sahi\scripts C:\Users\Administrator\sahi_pro\userdata\scripts
+xcopy /s C:\Users\Administrator\Documents\GitHub\circleci-sahi\scripts C:\Users\Administrator\sahi_pro\userdata\scripts
 
 :: This will execute the scripts
 cd C:\Users\Administrator\sahi_pro\userdata\bin && testrunner.bat testSmoke.suite https://qa.launchpadrecruitsapp.com/login ie
