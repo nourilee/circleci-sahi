1fccdc8e7d534a292a55dba4a8721d7ce9c04e5c
Nourilee <nourilee@gmail.com>
2019-02-06 10:36:55 +0800

    Trial #3 -- 20190602

--- testSahi.bat
+++
@@ -1,5 +0,0 @@
-:: This will copy scripts from repository to the sahi directory
-xcopy /s/y C:\Users\Administrator\Documents\GitHub\circleci-sahi\scripts C:\Users\Administrator\sahi_pro\userdata\scripts
-
-:: This will execute the scripts
-cd C:\Users\Administrator\sahi_pro\userdata\bin && testrunner.bat testSmoke.suite https://qa.launchpadrecruitsapp.com/login ie
