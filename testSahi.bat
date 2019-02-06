32b35ab002c4588ac7a9e0b751a8e70afd0edadc
Nourilee <nourilee@gmail.com>
2019-02-06 10:10:41 +0800

    Update testSahi.bat

--- testSahi.bat
+++ testSahi.bat
@@ -2,11 +2,11 @@
 cd C:\Program Files\Git\bin && start sh.exe
 
 :: This will execute git commands
-cd C:/Users/Administrator/Documents/GitHub/circleci-sahi && git remote -v 
+cd C:/Users/Administrator/Documents/GitHub/circleci-sahi && git pull origin master
 exit
 
 :: This will copy scripts from repository to the sahi directory
-xcopy /s C:\Users\Administrator\Documents\GitHub\circleci-sahi\scripts C:\Users\Administrator\sahi_pro\userdata\scripts
+xcopy /s/y C:\Users\Administrator\Documents\GitHub\circleci-sahi\scripts C:\Users\Administrator\sahi_pro\userdata\scripts
 
 :: This will execute the scripts
 cd C:\Users\Administrator\sahi_pro\userdata\bin && testrunner.bat testSmoke.suite https://qa.launchpadrecruitsapp.com/login ie
