056a6e29cb019f4e8bf7c772135b4dd3f0205437
Nourilee <nourilee@gmail.com>
2019-02-19 17:52:41 +0800

    Attempt to force fail

--- testSahi.bat
+++ testSahi.bat
@@ -3,9 +3,5 @@
 cd C:\Users\Administrator\sahi_pro\userdata\bin 
 testrunner.bat testSmoke.suite https://qa.launchpadrecruitsapp.com/login ie > C:\temp\testSmoke.log
 
-cd C:\Users\Administrator\sahi_pro\userdata\bin
 findstr FAILED C:\temp\testSmoke.log
-if %ERRORLEVEL%==0 (
-echo Some tests FAILED!
-exit 999
-)
+if %ERRORLEVEL%==0 exit 999
