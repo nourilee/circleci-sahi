7f61c2c247df86ff7d34630dc08b6ebd0a5ab12b
Nourilee <nourilee@gmail.com>
2019-02-19 17:38:15 +0800

    Attempt to force fail

--- testSahi.bat
+++ testSahi.bat
@@ -1,7 +1,7 @@
 @echo off
 
 cd C:\Users\Administrator\sahi_pro\userdata\bin 
-testrunner.bat testSmoke.suite https://qa.launchpadrecruitsapp.com/login ie" > C:\temp\testSmoke.log
+testrunner.bat testSmoke.suite https://qa.launchpadrecruitsapp.com/login ie > C:\temp\testSmoke.log
 
 cd C:\Users\Administrator\sahi_pro\userdata\bin
 findstr FAILED C:\temp\testSmoke.log
