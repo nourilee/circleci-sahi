dadbe4d43d00306f0e933ab37ace2fffec01c590
Nourilee <nourilee@gmail.com>
2019-02-19 16:47:18 +0800

    Attempt to force fail

--- testSahi.bat
+++ testSahi.bat
@@ -3,4 +3,5 @@ REM First attempt at checking return codes
 
 cd C:\Users\Administrator\sahi_pro\userdata\bin && testrunner.bat testFail.sah https://qa.launchpadrecruitsapp.com/login ie > temp.txt
 findstr FAILED temp.txt
-if %ERRORLEVEL%==1 EXIT 9
+if %ERRORLEVEL%==0 EXIT 9
+
